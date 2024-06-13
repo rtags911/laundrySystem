468c57dce165f575411901d9c49074360da5abb0
rtags911 <rtagiobon@hyperstacksinc.com>
2024-06-14 05:54:18 +0800

    test

--- admin/scripts/queue.js
+++ admin/scripts/queue.js
@@ -78,6 +78,29 @@ document.addEventListener("DOMContentLoaded", () => {
             },
           },
         ],
+        initComplete: function () {
+          this.api()
+            .columns(4)
+            .every(function () {
+              var column = this;
+              var select = $(
+                '<select><option value="">Select Status</option></select>'
+              )
+                .appendTo($(column.header()).empty())
+                .on("change", function () {
+                  var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                  column.search(val ? "^" + val + "$" : "", true, false).draw();
+                });
+
+              column
+                .data()
+                .unique()
+                .sort()
+                .each(function (d, j) {
+                  select.append('<option value="' + d + '">' + d + "</option>");
+                });
+            });
+        },
         paging: true,
         searching: true,
         info: true,
