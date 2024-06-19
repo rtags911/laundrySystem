1518b3ecb5ef9c718cf7d2667a295bfa5bc1fe92
rtags911 <rtagiobon@hyperstacksinc.com>
2024-06-19 08:29:23 +0800

    delete

--- admin/api/queue/index.php
+++ admin/api/queue/index.php
@@ -49,7 +49,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $action = $data['action'];
 
         if ($action == "laundry") {
-            delete($db, $data);
+            delete($db);
         }
     } else {
         removeQueue($db);
